ce06c989cae71cb7c47949d94f3c5731176ff4d0
Oliver Hepworth-Bell <Oliver Hepworth-Bell>
2016-02-05 13:17:42 +0000

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# oddball-magnifier
-A touch-friendly, light-weight image magnifier
+# Oddball Magnifier
+A touch-friendly, light-weight image magnifier. Can be used on multiple images on a page, and is highly customisable.
+
+![Screenshot of working Oddball Magnifier](http://wearegrowth.com/zoomer.jpg)
+
+
+### How to use the Oddball Magnifier
+To designate an image to be magnified:
+```
+<img src="example.jpg" class="your-class-name" />
+```
+
+Make sure you have jQuery called, thing simply add 
+```
+<!-- Remember to include jQuery -->
+<script src='./oddballMagnifier.min.js'></script>
+```
+
+To run the script:
+```
+<script>
+$('.your-class-name').oddballMagnifier();
+</script>
+```
+
+You can also designate a different magnified image (for example, a high-definition version of the image) like so:
+```
+<img src="example.jpg" data-hdsrc="example-hd.jpg" class="your-class-name" />
+```
+
+For more customisability, you can change the lens layout:
+```
+$('.your-class-name').oddballMagnifier({
+	lens: 340,
+	curve: 340,
+	shadow: 0.1,
+	spread: 30,
+	zoom: 1920,
+	border: 8
+});
+```
+
+
+### Documentation
+* lens -- Sets the size of the magnifier (pixels)
+* curve -- sets the border-radius of the lends (pixels)
+* shadow -- Sets the opacity of the drop-shadow of the magnifier (set from 0-1)
+* spread -- Sets the size/spread of the drop-shadow (pixels)
+* zoom -- Sets the size of the magnifier image (pixels)
+* border -- Sets the border-width of the magnifier
+
+
+### To do
+* Add more customisable shapes to the magnifier
+* Add ability to change border and shadow colours
+* Anything anyone else can think of/cares to add
+* Possible pre-loading of HD images
+* Transitions on lens to fade in and out
+
+
+### Dependencies
+* jQuery
---
+++ oddballMagnifier.js
@@ -0,0 +1,165 @@
+/**
+ * The Oddball Magnifier
+ * A touch-compatible, light-weight image magnifier
+ * -------------------------------------------------------------------
+ * @version 0.0.2
+ * @author Oliver Hepworth-Bell (@ohepworthbell)
+ * @license The MIT License (MIT)
+ * @todo Add more customisation (i.e. custom shapes and border colours)
+ */
+
+ 
+(function($){
+	jQuery.fn.oddballMagnifier = function(settings){
+		
+		/* Make sure only 1 lens is created */
+		var isreplica=false;	
+		
+		/* Set up default settings */
+		var config = {
+			lens: 340,		// Sets the size  of the magnifying lens (pixels)
+			curve: 340,		// Sets the border-radius of the lens (set to 0 for a square)
+			spread: 20,		// Sets the size/spread of the lens shadow (pixels)
+			shadow: 0.1,	// Sets the opacity of the shadow (between 0 and 1)
+			zoom: 1920,		// Sets the size of the zoomed in image (pixels)
+			border: 8		// Sets the border of the lens (pixels)
+		};
+		
+		/* Load in default settings */
+		if(settings){
+			$.extend(config, settings);
+		}
+		
+		/* Call the function for each element */
+		return this.each(function(){
+			
+			/* A few default variables */
+			var img=$(this),
+			    touch=false,
+				img,
+				mag,
+				bg,
+				bghd,
+				over;
+			
+			/* Set up for each magnifiying image */
+			if(img.length) {
+				$(this).attr('data-width', $(this).width());
+				$(this).attr('data-height', $(this).height());
+				
+				/* Create the lens (once!) */
+				if(!isreplica) {
+					isreplica=true;
+					$('body').append('<div class="oddball-lens"></div>');
+				}
+				
+				/* Apply default styles to the oddball lens (no external CSS files required */
+				$('.oddball-lens').css({
+					'position': 'fixed',
+					'top': '0',
+					'left': '0',
+					'z-index': '9999',
+					'width': config.lens + 'px',
+					'height': config.lens + 'px',
+					'background-clip': 'border-box',
+					'border-radius': config.curve + 'px',
+					'border': config.border+'px solid rgba(255,255,255,0.4)',
+					'background': '#eee no-repeat 0 0',
+					'background-size': config.zoom + 'px',
+					'box-shadow': '0 0 ' + config.spread + 'px rgba(0,0,0,' + config.shadow + ')',
+					'box-sizing': 'border-box',
+					'cursor': 'none',
+					'display': 'none'
+				});
+				
+				/* Check defaults on screen resize 
+				 * NOTE: I have kept 'scroll' in the check, as when rotating some mobile
+				 * devices, the 'resize' doesn't fire. Scroll is pretty hacky, but it works!
+				 */
+				$(window).on('load scroll resize', function() {
+					img.each(function() {
+						$(this).attr('data-width', $(this).width());
+						$(this).attr('data-height', $(this).height())
+					});
+				});
+				
+				/* Check for the start of any interaction */
+				img.on('mouseenter touchstart', function() {
+					over=true;
+					mag=$(this);
+					
+					bghd = $(this).attr('data-hdsrc');
+					
+					if(typeof bghd !== typeof undefined && bghd !== false) {
+						bg = $(this).attr('data-hdsrc');
+					}
+					else {
+						bg = $(this).attr('src');
+					}
+					
+					console.log(bg);
+					
+					$('.oddball-lens').css('background-image', 'url('+bg+')');
+					$('.oddball-lens').stop().show();
+				});
+				
+				/* Test to catch touch-devices */
+				img.on('touchstart', function() {
+					touch=true;
+				});
+				
+				/* Get mouse/finger positions and call function with results */
+				$(window).on('touchstart touchmove mousemove', function(e) {
+					if(over) {
+						if(touch) {
+							x = e.originalEvent.touches[0].pageX;
+							y = e.originalEvent.touches[0].pageY;
+						}
+						else {
+							x = e.pageX;
+							y = e.pageY;
+						}
+						doMove(x,y,mag);
+					}
+				});
+				
+				/* Function to calculate the lens position */
+				function doMove(x,y,mag) {
+					var offset = config.lens/2;
+					
+					var lenstop = mag.offset().top;
+					var left = mag.offset().left;
+					var width = parseInt(mag.attr('data-width'));
+					var height = parseInt(mag.attr('data-height'));
+					
+					/* test for mouse/finger position relative to image boundaries */
+					if (x < left || x > (left+width) || y < lenstop || y > (lenstop+height)) {
+						touch=false;
+						over=false;
+						mag=false;
+						
+						$('.oddball-lens').hide();
+					}
+					
+					/* Set background position as a percentage relative to lens position */
+					var bgpos = (100/width*(x-left)) + '% ' + (100/height*(y-lenstop)) + '%';
+					
+					$('.oddball-lens').css({
+						top: (y-offset) + 'px',
+						left: (x-offset) + 'px',
+						'background-position': bgpos
+					});
+				}
+				
+				/* Hide the lends on touchend */
+				$(window).on('touchend', function() {
+					touch=false;
+					over=false;
+					mag=false;
+					
+					$('.oddball-lens').hide();
+				});
+			}
+		});
+	};
+})(jQuery);
